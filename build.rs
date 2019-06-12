84900bae942358044f0bff96238ac62a382393c3
laser <l@s3r.me>
2019-06-12 16:29:51 -0700

    feat(drop unused crate type + move config into TOML)

--- build.rs
+++ build.rs
@@ -1,50 +1,7 @@
-extern crate cbindgen;
-
-use cbindgen::{Config, RenameRule};
-
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-
 fn main() {
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-
     println!("cargo:rerun-if-changed=src/");
 
-    let config = {
-        let mut c: Config = Default::default();
-
-        c.header = Some(
-            format!(
-                r##"
-/* filecoin_proofs Header Version {} */
-
-#ifdef __cplusplus
-extern "C" {{
-#endif
-"##,
-                VERSION
-            )
-            .trim()
-            .into(),
-        );
-
-        c.trailer = Some(
-            r##"
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
-"##
-            .trim()
-            .into(),
-        );
-
-        c.include_guard = Some("FILECOIN_PROOFS_CAPI_H".to_owned());
-        c.language = cbindgen::Language::C;
-        c.enumeration.rename_variants = Some(RenameRule::QualifiedScreamingSnakeCase);
-        c
-    };
-
-    // Generate filecoin_proofs.h.
-    cbindgen::generate_with_config(&crate_dir, config)
+    cbindgen::generate(std::env::var("CARGO_MANIFEST_DIR").unwrap())
         .expect("Could not generate header")
-        .write_to_file("include/filecoin_proofs.h");
+        .write_to_file("include/filecoin_proofs_ffi.h");
 }
