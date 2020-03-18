e697787aa031ce47350cf4b5755c5e2b7ce6bf55
laser <l@s3r.me>
2020-03-18 15:02:32 -0700

    force user to provide release output path

--- rust/scripts/package-release.sh
+++ rust/scripts/package-release.sh
@@ -2,28 +2,31 @@
 
 set -x
 
-if [ -z "$1" ]; then
-  TARBALL_OUTPUT_PATH=`mktemp`.tar.gz
-else
-  TARBALL_OUTPUT_PATH=$1
+if [[ -z "$1" ]]
+then
+    (>&2 echo 'Error: script requires path to which it will write release (gzipped) tarball, e.g. "/tmp/filecoin-ffi-Darwin-standard.tar.tz"')
+    exit 1
 fi
+TARBALL_OUTPUT_PATH=$1
 
 echo "preparing release file"
 
-TMP_DIR=`mktemp -d`
+# create temporary directory to hold build artifacts
+#
+TMP_DIR=$(mktemp -d)
 
-mkdir -p $TMP_DIR
+# clean up temp directory on exit
+#
+trap '{ rm -f $TMP_DIR; }' EXIT
 
+# copy assets into temporary directory
+#
 find -L . -type f -name filecoin.h -exec cp -- "{}" $TMP_DIR/ \;
 find -L . -type f -name libfilecoin.a -exec cp -- "{}" $TMP_DIR/ \;
 find -L . -type f -name filecoin.pc -exec cp -- "{}" $TMP_DIR/ \;
 
-pushd $TMP_DIR
-
-tar -czf $TARBALL_OUTPUT_PATH ./*
+# create gzipped tarball from contents of temporary directory
+#
+tar -czf $TARBALL_OUTPUT_PATH $TMP_DIR/*
 
 echo "release file created: $TARBALL_OUTPUT_PATH"
-
-popd
-
-rm -rf $TMP_DIR
