d680724393022b7a297bbd912554446177d642c3
laser <l@s3r.me>
2020-03-19 08:26:30 -0700

    write diagnostic logging to stderr, change flags, lowercase variables

--- rust/scripts/package-release.sh
+++ rust/scripts/package-release.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-set -x
+set -Exeuo pipefail
 
 if [[ -z "$1" ]]
 then
     (>&2 echo 'Error: script requires path to which it will write release (gzipped) tarball, e.g. "/tmp/filecoin-ffi-Darwin-standard.tar.tz"')
     exit 1
 fi
-TARBALL_OUTPUT_PATH=$1
+tarball_output_path=$1
 
-echo "preparing release file"
+(>&2 echo "preparing release files")
 
 # create temporary directory to hold build artifacts
 #
-TMP_DIR=$(mktemp -d)
+tmp_dir=$(mktemp -d)
 
 # clean up temp directory on exit
 #
-trap '{ rm -f $TMP_DIR; }' EXIT
+trap '{ rm -f $tmp_dir; }' EXIT
 
 # copy assets into temporary directory
 #
-find -L . -type f -name filecoin.h -exec cp -- "{}" $TMP_DIR/ \;
-find -L . -type f -name libfilecoin.a -exec cp -- "{}" $TMP_DIR/ \;
-find -L . -type f -name filecoin.pc -exec cp -- "{}" $TMP_DIR/ \;
+find -L . -type f -name filecoin.h -exec cp -- "{}" $tmp_dir/ \;
+find -L . -type f -name libfilecoin.a -exec cp -- "{}" $tmp_dir/ \;
+find -L . -type f -name filecoin.pc -exec cp -- "{}" $tmp_dir/ \;
 
 # create gzipped tarball from contents of temporary directory
 #
-tar -czf $TARBALL_OUTPUT_PATH $TMP_DIR/*
+tar -czf $tarball_output_path $tmp_dir/*
 
-echo "release file created: $TARBALL_OUTPUT_PATH"
+(>&2 echo "release file created: $tarball_output_path")
