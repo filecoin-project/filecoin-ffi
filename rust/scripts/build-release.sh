cb6319aabec4331236ee873b35b395aeaa30c303
laser <l@s3r.me>
2020-03-19 11:29:56 -0700

    prefix variables with local, put stuff in main, make sure we rm -rf the directory to be archived

--- rust/scripts/build-release.sh
+++ rust/scripts/build-release.sh
@@ -2,49 +2,53 @@
 
 set -Exeo pipefail
 
-if [[ -z "$1" ]]
-then
-    (>&2 echo 'Error: script requires a library name, e.g. "filecoin" or "snark"')
-    exit 1
-fi
+main() {
+    if [[ -z "$1" ]]
+    then
+        (>&2 echo '[build-release/main] Error: script requires a library name, e.g. "filecoin" or "snark"')
+        exit 1
+    fi
 
-if [[ -z "$2" ]]
-then
-    (>&2 echo 'Error: script requires a toolchain, e.g. ./build-release.sh +nightly-2019-04-19')
-    exit 1
-fi
+    if [[ -z "$2" ]]
+    then
+        (>&2 echo '[build-release/main] Error: script requires a toolchain, e.g. ./build-release.sh +nightly-2019-04-19')
+        exit 1
+    fi
 
-build_output_tmp=$(mktemp)
+    local __build_output_log_tmp=$(mktemp)
 
-# clean up temp file on exit
-#
-trap '{ rm -f $build_output_tmp; }' EXIT
+    # clean up temp file on exit
+    #
+    trap '{ rm -f $__build_output_log_tmp; }' EXIT
 
-# build with RUSTFLAGS configured to output linker flags for native libs
-#
-rust_flags="${RUSTFLAGS}"
-rust_flags="--print native-static-libs ${rust_flags}"
+    # build with RUSTFLAGS configured to output linker flags for native libs
+    #
+    local __rust_flags="${RUSTFLAGS}"
+    __rust_flags="--print native-static-libs ${__rust_flags}"
 
-RUSTFLAGS="${rust_flags}" \
-    cargo +$2 build \
-    --release ${@:3} 2>&1 | tee ${build_output_tmp}
+    RUSTFLAGS="${__rust_flags}" \
+        cargo +$2 build \
+        --release ${@:3} 2>&1 | tee ${__build_output_log_tmp}
 
-# parse build output for linker flags
-#
-linker_flags=$(cat ${build_output_tmp} \
-    | grep native-static-libs\: \
-    | head -n 1 \
-    | cut -d ':' -f 3)
+    # parse build output for linker flags
+    #
+    local __linker_flags=$(cat ${__build_output_log_tmp} \
+        | grep native-static-libs\: \
+        | head -n 1 \
+        | cut -d ':' -f 3)
 
-# generate pkg-config
-#
-sed -e "s;@VERSION@;$(git rev-parse HEAD);" \
-    -e "s;@PRIVATE_LIBS@;${linker_flags};" "$1.pc.template" > "$1.pc"
+    # generate pkg-config
+    #
+    sed -e "s;@VERSION@;$(git rev-parse HEAD);" \
+        -e "s;@PRIVATE_LIBS@;${__linker_flags};" "$1.pc.template" > "$1.pc"
 
-# ensure header file was built
-#
-find -L . -type f -name "$1.h" | read
+    # ensure header file was built
+    #
+    find -L . -type f -name "$1.h" | read
 
-# ensure the archive file was built
-#
-find -L . -type f -name "lib$1.a" | read
+    # ensure the archive file was built
+    #
+    find -L . -type f -name "lib$1.a" | read
+}
+
+main "$@"; exit
--- rust/scripts/package-release.sh
+++ rust/scripts/package-release.sh
@@ -2,31 +2,36 @@
 
 set -Exeuo pipefail
 
-if [[ -z "$1" ]]
-then
-    (>&2 echo 'Error: script requires path to which it will write release (gzipped) tarball, e.g. "/tmp/filecoin-ffi-Darwin-standard.tar.tz"')
-    exit 1
-fi
-tarball_output_path=$1
+main() {
+    if [[ -z "$1" ]]
+    then
+        (>&2 echo '[package-release/main] Error: script requires path to which it will write release (gzipped) tarball, e.g. "/tmp/filecoin-ffi-Darwin-standard.tar.tz"')
+        exit 1
+    fi
 
-(>&2 echo "preparing release files")
+    local __tarball_output_path=$1
 
-# create temporary directory to hold build artifacts
-#
-tmp_dir=$(mktemp -d)
+    # create temporary directory to hold build artifacts
+    #
+    local __tmp_dir=$(mktemp -d)
 
-# clean up temp directory on exit
-#
-trap '{ rm -f $tmp_dir; }' EXIT
+    (>&2 echo "[package-release/main] preparing release files")
 
-# copy assets into temporary directory
-#
-find -L . -type f -name filecoin.h -exec cp -- "{}" $tmp_dir/ \;
-find -L . -type f -name libfilecoin.a -exec cp -- "{}" $tmp_dir/ \;
-find -L . -type f -name filecoin.pc -exec cp -- "{}" $tmp_dir/ \;
+    # clean up temp directory on exit
+    #
+    trap '{ rm -rf $__tmp_dir; }' EXIT
 
-# create gzipped tarball from contents of temporary directory
-#
-tar -czf $tarball_output_path $tmp_dir/*
+    # copy assets into temporary directory
+    #
+    find -L . -type f -name filecoin.h -exec cp -- "{}" $__tmp_dir/ \;
+    find -L . -type f -name libfilecoin.a -exec cp -- "{}" $__tmp_dir/ \;
+    find -L . -type f -name filecoin.pc -exec cp -- "{}" $__tmp_dir/ \;
 
-(>&2 echo "release file created: $tarball_output_path")
+    # create gzipped tarball from contents of temporary directory
+    #
+    tar -czf $__tarball_output_path $__tmp_dir/*
+
+    (>&2 echo "[package-release/main] release file created: $__tarball_output_path")
+}
+
+main "$@"; exit
