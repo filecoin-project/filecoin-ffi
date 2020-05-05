a5b9e249a3ac502dc93efc9ce49884253f7a8cb0
laser <l@s3r.me>
2020-05-05 10:16:03 -0700

    rename symbols within custom allocate methods

--- generated/customallocs.go
+++ generated/customallocs.go
@@ -12,36 +12,43 @@ import (
 	"unsafe"
 )
 
+// AllocateProxy allocates a FilPrivateReplicaInfo proxy object in the C heap,
+// returning a function which, when called, frees the allocated memory. This
+// method exists because the default c-for-go allocation strategy allocates a
+// C struct with a field whose values is a pointer into the Go heap, which is
+// not permitted by the most strict CGO check (cgocheck=2).
 func (x *FilPrivateReplicaInfo) AllocateProxy() func() {
-	mem81a31e9b := allocFilPrivateReplicaInfoMemory(1)
-	ref81a31e9b := (*C.fil_PrivateReplicaInfo)(mem81a31e9b)
-	ref81a31e9b.cache_dir_path = C.CString(x.CacheDirPath)
-	ref81a31e9b.comm_r = *(*[32]C.uint8_t)(unsafe.Pointer(&x.CommR))
-	ref81a31e9b.registered_proof = (C.fil_RegisteredPoStProof)(x.RegisteredProof)
-	ref81a31e9b.replica_path = C.CString(x.ReplicaPath)
-	ref81a31e9b.sector_id = (C.uint64_t)(x.SectorId)
+	mem := allocFilPrivateReplicaInfoMemory(1)
+	proxy := (*C.fil_PrivateReplicaInfo)(mem)
+	proxy.cache_dir_path = C.CString(x.CacheDirPath)
+	proxy.comm_r = *(*[32]C.uint8_t)(unsafe.Pointer(&x.CommR))
+	proxy.registered_proof = (C.fil_RegisteredPoStProof)(x.RegisteredProof)
+	proxy.replica_path = C.CString(x.ReplicaPath)
+	proxy.sector_id = (C.uint64_t)(x.SectorId)
 
-	x.ref81a31e9b = ref81a31e9b
+	x.ref81a31e9b = proxy
 
 	return func() {
-		C.free(unsafe.Pointer(ref81a31e9b.cache_dir_path))
-		C.free(unsafe.Pointer(ref81a31e9b.replica_path))
-		C.free(unsafe.Pointer(ref81a31e9b))
+		C.free(unsafe.Pointer(proxy.cache_dir_path))
+		C.free(unsafe.Pointer(proxy.replica_path))
+		C.free(unsafe.Pointer(proxy))
 	}
 }
 
+// AllocateProxy allocates a FilPoStProof proxy object in the C heap,
+// returning a function which, when called, frees the allocated memory.
 func (x *FilPoStProof) AllocateProxy() func() {
-	mem3451bfa := allocFilPoStProofMemory(1)
-	ref3451bfa := (*C.fil_PoStProof)(mem3451bfa)
+	mem := allocFilPoStProofMemory(1)
+	proxy := (*C.fil_PoStProof)(mem)
 
-	ref3451bfa.registered_proof = (C.fil_RegisteredPoStProof)(x.RegisteredProof)
-	ref3451bfa.proof_len = (C.size_t)(x.ProofLen)
-	ref3451bfa.proof_ptr = (*C.uchar)(unsafe.Pointer(C.CString(x.ProofPtr)))
+	proxy.registered_proof = (C.fil_RegisteredPoStProof)(x.RegisteredProof)
+	proxy.proof_len = (C.size_t)(x.ProofLen)
+	proxy.proof_ptr = (*C.uchar)(unsafe.Pointer(C.CString(x.ProofPtr)))
 
-	x.ref3451bfa = ref3451bfa
+	x.ref3451bfa = proxy
 
 	return func() {
-		C.free(unsafe.Pointer(ref3451bfa.proof_ptr))
-		C.free(unsafe.Pointer(ref3451bfa))
+		C.free(unsafe.Pointer(proxy.proof_ptr))
+		C.free(unsafe.Pointer(proxy))
 	}
 }
