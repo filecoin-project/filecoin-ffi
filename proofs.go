79b3249666a809871fad12fbf50f68e22218b01c
whyrusleeping <why@ipfs.io>
2020-09-09 13:52:47 -0700

    return faulty sectors from GenerateWinningPoSt call

--- proofs.go
+++ proofs.go
@@ -17,6 +17,7 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
 	"github.com/ipfs/go-cid"
 	"github.com/pkg/errors"
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/filecoin-ffi/generated"
 )
@@ -557,16 +558,16 @@ func GenerateWindowPoSt(
 	minerID abi.ActorID,
 	privateSectorInfo SortedPrivateSectorInfo,
 	randomness abi.PoStRandomness,
-) ([]proof.PoStProof, error) {
+) ([]proof.PoStProof, []abi.SectorNumber, error) {
 	filReplicas, filReplicasLen, free, err := toFilPrivateReplicaInfos(privateSectorInfo.Values(), "window")
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to create private replica info array for FFI")
+		return nil, nil, errors.Wrap(err, "failed to create private replica info array for FFI")
 	}
 	defer free()
 
 	proverID, err := toProverID(minerID)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	resp := generated.FilGenerateWindowPost(to32ByteArray(randomness), filReplicas, filReplicasLen, proverID)
@@ -577,15 +578,20 @@ func GenerateWindowPoSt(
 	defer generated.FilDestroyGenerateWindowPostResponse(resp)
 
 	if resp.StatusCode != generated.FCPResponseStatusFCPNoError {
-		return nil, errors.New(generated.RawString(resp.ErrorMsg).Copy())
+		return nil, nil, errors.New(generated.RawString(resp.ErrorMsg).Copy())
 	}
 
 	proofs, err := fromFilPoStProofs(resp.ProofsPtr)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return proofs, nil
+	faultySectors, err := fromFilPoStFaultySectors(resp.FaultySectorsPtr, resp.FaultySectorsLen)
+	if err != nil {
+		return nil, nil, xerrors.Errorf("failed to parse faulty sectors list: %w", err)
+	}
+
+	return proofs, faultySectors, nil
 }
 
 // GetGPUDevices produces a slice of strings, each representing the name of a
@@ -795,6 +801,19 @@ func toFilPrivateReplicaInfos(src []PrivateSectorInfo, typ string) ([]generated.
 			frees[idx]()
 		}
 	}, nil
+}
+
+func fromFilPoStFaultySectors(ptr []uint64, l uint) ([]abi.SectorNumber, error) {
+	if l == 0 {
+		return nil, nil
+	}
+
+	snums := make([]abi.SectorNumber, 0, l)
+	for i := uint(0); i < l; i++ {
+		snums = append(snums, abi.SectorNumber(ptr[i]))
+	}
+
+	return snums, nil
 }
 
 func fromFilPoStProofs(src []generated.FilPoStProof) ([]proof.PoStProof, error) {
