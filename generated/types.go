9eb8960d3b6a2b359e43a291feecabca3edf526f
nemo <nemo@protocol.ai>
2020-10-14 13:00:24 -0400

    fix: make cgo-gen and add RUSTFLAGS to go tests

--- generated/types.go
+++ generated/types.go
@@ -4,7 +4,7 @@
 package generated
 
 /*
-#cgo LDFLAGS: -L${SRCDIR}/.. -lfilcrypto -L/tmp/__hwloc2/lib -lhwloc
+#cgo LDFLAGS: -L${SRCDIR}/.. -lfilcrypto -L/usr/local/fil-hwloc/lib -lhwloc
 #cgo pkg-config: ${SRCDIR}/../filcrypto.pc --static
 #include "../filcrypto.h"
 #include <stdlib.h>
