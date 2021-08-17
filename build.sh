f736c71bd662b6c4a5db19d1cc2e56a548106b73
nemo <nemo@protocol.ai>
2021-08-17 08:18:07 -0400

    fix: run apt update before updating via apt-get

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -e
 make clean
 cd rust
 rm -f Cargo.lock
+rustup target add x86_64-apple-darwin --toolchain $(cat rust-toolchain)
+rustup target add aarch64-apple-darwin --toolchain $(cat rust-toolchain)
 cargo update -p "filecoin-proofs-api"
 cargo install cbindgen
 cbindgen --clean --config cbindgen.toml --crate filcrypto --output ../include/filcrypto.h
