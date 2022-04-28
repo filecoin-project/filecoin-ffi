af4846c1d40fa046d08c95f783cf2a8e812d04b6
Raúl Kripalani <raul@protocol.ai>
2022-04-28 23:31:09 +0200

    exec stats: print out num_syscalls, num_externs.

--- rust/src/fvm/machine.rs
+++ rust/src/fvm/machine.rs
@@ -247,7 +247,7 @@ pub unsafe extern "C" fn fil_fvm_machine_execute_message(
                 .map(|a| a.code);
             let _ = writeln!(
                 log,
-                r#"{{"type":"apply","epoch":{},"fuel":{},"wasm_time":{},"call_overhead":{},"gas":{},"compute_gas":{},"time":{},"code":{},"method":{}}}"#,
+                r#"{{"type":"apply","epoch":{},"fuel":{},"wasm_time":{},"call_overhead":{},"gas":{},"compute_gas":{},"num_syscalls":{},"num_externs":{},time":{},"code":{},"method":{}}}"#,
                 executor.context().epoch,
                 stats.fuel_used,
                 stats.wasm_duration.as_nanos(),
@@ -261,6 +261,8 @@ pub unsafe extern "C" fn fil_fvm_machine_execute_message(
                 },
                 apply_ret.msg_receipt.gas_used,
                 stats.compute_gas,
+                stats.num_syscalls,
+                stats.num_externs,
                 duration.as_nanos(),
                 code.map(|c| format!(r#""{}""#, c))
                     .unwrap_or_else(|| String::from("null")),
