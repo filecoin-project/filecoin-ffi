a7e57dfb357d676d4c29884b538689fe8b6482d9
vyzo <vyzo@hackzen.org>
2022-05-05 19:31:02 +0300

    enable actor debugging

--- rust/src/fvm/machine.rs
+++ rust/src/fvm/machine.rs
@@ -109,7 +109,8 @@ fn create_fvm_machine(
 
             machine_context
                 .set_base_fee(base_fee)
-                .set_circulating_supply(base_circ_supply);
+                .set_circulating_supply(base_circ_supply)
+                .enable_actor_debugging();
 
             if tracing {
                 machine_context.enable_tracing();
