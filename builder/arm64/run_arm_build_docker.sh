4a345ad6d856117790cd045b564163a10b1a685d
alvin-reyes <me@areyes.onl>
2022-05-11 10:51:12 -0400

    chore: added arm64 scripts

--- builder/arm64/run_arm_build_docker.sh
+++ builder/arm64/run_arm_build_docker.sh
@@ -4,9 +4,11 @@ timestamp=$(date +%s)
 name=arm_build_only_$timestamp
 docker build -t $name .
 
+# get the container id
 id=$(docker create $name)
-docker cp $id:/filecoin-ffi/ - > $name.tar.gz
 docker cp $id:/filecoin-ffi/ - > $name.tar.gz
 
 ## extract 
 tar -xvf $name.tar.gz
+
+## Copy to wherever..
