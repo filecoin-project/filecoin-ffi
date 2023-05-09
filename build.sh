66c08045754404514b0f87f9ac1d19ea563e01b5
nemo <nemo@protocol.ai>
2023-05-09 12:33:04 -0400

    feat: update dependencies to WIP synthetic-porep code

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -e
 make clean
 cd rust
 rm -f Cargo.lock
-rustup target add x86_64-apple-darwin --toolchain $(cat rust-toolchain)
-rustup target add aarch64-apple-darwin --toolchain $(cat rust-toolchain)
+rustup target add x86_64-apple-darwin
+rustup target add aarch64-apple-darwin
 cargo update -p "filecoin-proofs-api"
 cargo install cargo-lipo
 cd ..
