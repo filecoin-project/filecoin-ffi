ed530c292e5c91025e7304e26efa63314d21cc9d
Phi <orjan.roren@gmail.com>
2024-04-13 11:04:02 -0500

    Add nv23 skeleton
    Add nv23 skeleton

--- rust/src/fvm/engine.rs
+++ rust/src/fvm/engine.rs
@@ -70,8 +70,8 @@ impl TryFrom<u32> for EngineVersion {
         match value {
             16 | 17 => Ok(EngineVersion::V1),
             18..=20 => Ok(EngineVersion::V2),
-            21 | 22 => Ok(EngineVersion::V3),
-            _ => Err(anyhow!("network version not supported")),
+            21..=23 => => Ok(EngineVersion::V3),
+            _ => return Err(anyhow!("network version not supported")),
         }
     }
 }
